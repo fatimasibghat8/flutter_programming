0f0febda362efe6ff120ce91ad23848cdacbf300
fatimasibghat8 <145971109+fatimasibghat8@users.noreply.github.com>
2023-09-25 13:27:08 +0500

    Add files via upload

---
+++ dart_practice01.dart
@@ -0,0 +1,35 @@
+import 'dartio';
+void main() {
+  print('my first dart project');
+  for (int i = 0; i  5; i++) {
+    print('hello ${i + 1}');
+  }
+ var name='fatima';
+  var name =stdin.readLineSync();
+   print(name $name);
+  student(name);
+  print(updated student name= $name);
+  different types of variable declaration
+  int a=5;not NULL by default we should assign some value to it
+  int age;it is nullable
+ 
+  int age=2;
+   print(age);
+  
+
+  
+  
+}
+class student
+{
+  student(var n)
+  { 
+    n=fatima sibghat;
+    print(updated student name= $n);
+}
+    
+ void studentage(int age)
+ {
+  age=21;
+}
+}
